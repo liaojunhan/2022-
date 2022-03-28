acf84e467e123d3e3632a1717f86cca14d0034a5
liaojunhan <64124082+liaojunhan@users.noreply.github.com>
2022-03-28 16:32:04 +0800

    Add files via upload

--- week06_keyboard_mouse_motion/main.cpp
+++ week06_keyboard_mouse_motion/main.cpp
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <GL/glut.h>
-float x=0,y=0,z=0,scale=1.0,angle=0.0,oldx,oldy;
+float x=250,y=250,z=0,scale=1.0,angle=0.0,oldx,oldy;
 int now=1;
 void display()
 {
+    glClearColor(0.5,0.5,0.5,1);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glPushMatrix();
-        glTranslatef((x-150)/150.0,-(y-150)/150.0,z);
+        glTranslatef((x-250)/250.0,-(y-250)/250.0,z);
         glRotatef(angle,0,0,1);
         glScalef(scale,scale,scale);
         glColor3f(1,1,0);
@@ -32,6 +33,10 @@ void motion(int mousex,int mousey)
         x+=(mousex-oldx);
         y+=(mousey-oldy);
     }
+    else if(now==2)
+    {
+        angle+=(mousex-oldx);
+    }
     else if(now==3)
     {
         if(mousex>oldx) scale*=1.01;
@@ -44,6 +49,7 @@ int main( int argc, char**argv )
 {
     glutInit( &argc, argv);
     glutInitDisplayMode( GLUT_DOUBLE | GLUT_DEPTH );
+    glutInitWindowSize(500,500);
     glutCreateWindow("week06 keyboard_mouse_motion");
     glutDisplayFunc(display);
     glutKeyboardFunc(keyboard);
